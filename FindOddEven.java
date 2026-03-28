dd23ebfb374cb631dac5940377a592f70d0b4cf1
vikashgupta9 <brijeshshaw9000@gmail.com>
2026-03-28 11:07:25 +0530

    all file added

---
+++ FindOddEven.java
@@ -0,0 +1,11 @@
+public class FindOddEven {
+    public static void main(String[] args) {
+        int number = 15;
+        
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+public class PrimeNumber {
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 100:");
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
