d10abc0ff3bd3eac08fec7fb3c223186df21c7d7
vikashgupta9 <brijeshshaw9000@gmail.com>
2026-03-28 11:56:15 +0530

    added

---
+++ f1.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    """Check if a number is prime."""
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+# Example usage
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
---
+++ f2.py
@@ -0,0 +1,8 @@
+# Odd Even Number Program
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number")
+else:
+    print(f"{num} is an odd number")
